d1ac18e4a9d2b89a79bc2ee32c588d99944e64a3
mprallison <m.p.rallison@gmail.com>
2026-02-12 10:16:52 +0000

    emily

--- app.py
+++ app.py
@@ -3,18 +3,26 @@ import csv
 
 env = Environment(loader=FileSystemLoader("templates"))
 
-#read data as jsonl
-with open("data.csv", mode="r", encoding="utf-8") as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        data = [row for row in csv_reader]
+data_file = "gormleys - Sheet1.csv"
+user = "emily"
+title = "emilyGormleyBagger"
+filename = "gormley_map_emily.html"
 
-#declare title
-title = "gormleyBagger"
-filename = "gormley_map.html"
+#read gormley data and user field
+#rename user field "status"
+keys = ["id", "name", "year", "location", "latitude", "longitude", "url", user]
+
+with open(data_file, mode="r", encoding="utf-8") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    data = [
+        {{user: "status"}.get(k, k): row[k] for k in keys} 
+        for row in csv_reader
+        ]
+
 
 #render template
 template = env.get_template("index.html")
-html = template.render(title=title, data=data)
+html = template.render(title=title, data=data, user=user)
 
 with open(filename, "w", encoding="utf-8") as f:
     f.write(html)
